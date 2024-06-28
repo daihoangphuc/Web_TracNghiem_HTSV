263d8ed9536f116e9dfaaab29c25dbf37a4677bd
phuc <nguyendaihoangphuc24@gmail.com>
2024-06-28 10:07:39 +0700

    Fix Register v2

--- obj/Debug/net8.0/Web_TracNghiem_HTSV.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web_TracNghiem_HTSV.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_TracNghiem_HTSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee028316c9ba25e3de01be865da102fae3db626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a419243e4d5db66fb9057a9b5148e5a2d822d493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_TracNghiem_HTSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_TracNghiem_HTSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
